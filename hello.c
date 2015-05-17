473a237585222d9be0fb87fac2e44e65a7def673
zelderus <zelder.rus@gmail.com>
2015-05-17 22:01:42 +0300

    file write

--- hello.c
+++ hello.c
@@ -2,38 +2,50 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+
 
 int print_hello (void)
 {
 	printf("Hello go..\n");
 }
-void p (char* str)
+void p (const char* str)
 {
 	printf("%s\n", str);
 }
 
-void write_demo(char* str)
+void write_demo(const char* str)
 {
 	int fd;
 	// file opts
-	char* fname = "kii.txt";
+	char* fname = "./kii.txt";
 	int fflags = O_CREAT | O_APPEND;
 	mode_t fmode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 	// open
+	errno = 0;
 	fd = open(fname, fflags, fmode);
 	if (fd == -1)
 	{
 		// err
-		printf("E: file open err");
+		printf("E: file open err -> %s\n", strerror(errno));
 		return;
 	}
+	printf("M: file opened with fd=%d\n", fd);
 	// write
-	
+	ssize_t nr;
+	errno = 0;
+	nr = write(fd, str, strlen(str));
+	if (nr == -1)
+	{
+		printf("E: file write err -> %s\n", strerror(errno));
+	}
 
 	// close
 	if (close(fd) == -1)
 	{
-		printf("E: file close err");
+		printf("E: file close err\n");
 		return;
 	}
 }
--- hello.h
+++ hello.h
@@ -1,4 +1,4 @@
 
 void print_hello(void);
-void p(char* str);
-void write_demo(char* str);
+void p(const char* str);
+void write_demo(const char* str);
